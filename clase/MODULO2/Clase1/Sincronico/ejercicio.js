c48cd99bf471a50cec577f0350519d677e91d035
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-04-20 23:59:14 -0400

    Ejercicio clase 1 modulo 2

---
+++ clase/MODULO2/Clase1/Sincronico/ejercicio.js
@@ -0,0 +1,8 @@
+
+    var userName = prompt('Buenos dias! Ingresa tu nombre');
+    var saludo = alert(' Bienvenido ' + userName)
+    var edad = prompt('¿Cuál es tu edad?');
+    console.log('Tu edad es: '+ edad)
+    document.getElementById('holi').innerHTML = 'Tú nombre es: ' + userName + ' y tu edad es: ' + edad
+   
+  
---
+++ clase/MODULO2/Clase1/Sincronico/ejercicio2.js
@@ -0,0 +1,7 @@
+var getData = () => {
+    var yourName = document.getElementById('textArea').value
+   
+    document.getElementById('lenght').innerHTML= 'Tu nombre tiene ' + yourName.length + ' caracteres';
+}
+
+

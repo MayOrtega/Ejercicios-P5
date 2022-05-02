4438be77658cfbe570a8ccd6774b39bb55d44f2f
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-05-02 11:49:01 -0400

    update

--- clase/MODULO2/examen-modulo2/app.js
+++ clase/MODULO2/examen-modulo2/app.js
@@ -1,7 +1,6 @@
 const words = [
     'californication',
-    ,
-    'black',
+      'black',
     'summer',
     'flea',
     'aeroplane',
@@ -20,17 +19,17 @@ const words = [
     'chili'
     ];
 
+    
     var palabraAleatoria = words[Math.floor(Math.random() * words.length)]
-
-    function randomWords () {
-
-        var time = 10;
-        var score = 0;            
+    var time = 10;
+    var score = 0;
+    
+     
    
-        
-        console.log(palabraAleatoria)
-
-        
+          
+       function randomWords () {
+       console.log(palabraAleatoria)
+      
     }
     
     randomWords()  
@@ -39,8 +38,57 @@ const words = [
         var wordRamdom = document.querySelector("#randomWord")
         wordRamdom.innerHTML=palabraAleatoria;
     }
-
+    
     addToDom()
 
+ 
+    //contador de tiempo
+    window.onload = actualizarTiempo;
+    function actualizarTiempo() {
+        document.querySelector('#timeSpan').innerHTML = time;
+        if(time==0){
+          
+        }else{
+          time-=1;
+          setTimeout("actualizarTiempo()",1500);
+        }
+      }
+
+
+      function updateScore(){
+        var totalScore = document.querySelector('#score').innerHTML = score+=1;
+        
+      }
+
+      function gameOver(){
+        document.querySelector('#end-game-container').innerHTML = 'Fin de la partida. Su puntaje total es: ' + totalScore;
+        
+
+        //version 2.0
+  
+    var input = document.querySelector('#text')
+
+    input.addEventListener('keypress', e => {
+      if(e.key == "Enter"){
+      var palabraIngresada = document.querySelector('#text').value
+      
+      
+       console.log(palabraIngresada)
+      }
+      
+      if(palabraIngresada == palabraAleatoria){
+
+       time+=4 
+       updateScore()
+       
+       
+
+                  
+     }else if(e.key == "Enter" && palabraAleatoria !==palabraIngresada ){
+       alert('Escriba bien')
+     }
+    
+  })
+  }
     
     
